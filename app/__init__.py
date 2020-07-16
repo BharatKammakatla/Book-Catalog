db0f3fd81410c984189c06768684a61cbc549c3c
Bharat Kammakatla <bharatkammakatla@gmail.com>
2020-07-17 00:37:58 +0800

    minor update

--- app/__init__.py
+++ app/__init__.py
@@ -20,6 +20,5 @@ def create_app(config_type):    #dev, test, prod
     from app.catalog import main    #import blueprint
     app.register_blueprint(main)    #register blueprint
 
-
     return app
 
