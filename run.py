e496069336fb08a9a520e1f92caf7bef605116d8
Bharat Kammakatla <bharatkammakatla@gmail.com>
2020-07-18 00:50:06 +0800

    Reverted to fix deployment issue with heroku

--- run.py
+++ run.py
@@ -1,13 +1,13 @@
 from app import create_app, db
 from app.auth.models import User
 
-if __name__=='__main__':
-    flask_app = create_app('prod')
-    with flask_app.app_context():
-        db.create_all()
-        # create default user (if not created)
-        if not User.query.filter_by(user_name='default').first():
-            User.create_user(user='default',
-                             email='default@user.com',
-                             password='Abcd!1234')
-    flask_app.run()
+# if __name__=='__main__':  # Include while using on dev env
+flask_app = create_app('prod')
+with flask_app.app_context():
+    db.create_all()
+    # create default user (if not created)
+    if not User.query.filter_by(user_name='default').first():
+        User.create_user(user='default',
+                         email='default@user.com',
+                         password='Abcd!1234')
+flask_app.run()
