46d4c37e749197c570a0ed59968c59bc644a340b
Bharat Kammakatla <bharatkammakatla@gmail.com>
2020-07-18 00:12:22 +0800

    Updated prod env variables

--- config/prod.py
+++ config/prod.py
@@ -1,4 +1,6 @@
+import os
+
 DEBUG = False
 SECRET_KEY = 'admin'
-SQLALCHEMY_DATABASE_URI  = 'postgresql://postgres:admin@localhost/catalog_db'
+SQLALCHEMY_DATABASE_URI  = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
