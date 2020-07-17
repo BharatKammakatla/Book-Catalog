a132d0ec6fafcabb3968024abf1edf67d64aee76
Bharat Kammakatla <bharatkammakatla@gmail.com>
2020-07-18 01:31:07 +0800

    updated run.py

--- run.py
+++ run.py
@@ -6,8 +6,8 @@ flask_app = create_app('prod')
 with flask_app.app_context():
     db.create_all()
     # create default user (if not created)
-    if not User.query.filter_by(user_name='default').first():
-        User.create_user(user='default',
-                         email='default@user.com',
-                         password='Abcd!1234')
+    if not User.query.filter_by(user_name='admin').first():
+        User.create_user(user='admin',
+                         email='admin@user.com',
+                         password='secret')
         flask_app.run()
